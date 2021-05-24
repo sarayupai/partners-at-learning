fc185c498009a4626ee8e2f1397e1213a3da552a
Sarayu Pai <sarujpai@gmail.com>
2021-05-24 14:21:31 -0700

    updated intro text

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -8,7 +8,7 @@ function HeroSection() {
         <div className='hero-container'>
             <h1>Sarayu's Math Corner</h1>
             <p>Welcome to Sarayu's Math Corner, a website of curated educational and enrichment materials for the remote learning of algebra!</p>
-            <h2>A message for you ...</h2>
+            <h2>A message to you ...</h2>
             <h3>Students</h3>
             <p>I know math can be frustrating, especially in this new age of remote learning, but hang in there! It's important to remember to take breaks and that it's okay if you don't always get something right on the first (or even the tenth) time as long as you keep trying. I believe in you :)</p>
             <h3>Teachers</h3>
